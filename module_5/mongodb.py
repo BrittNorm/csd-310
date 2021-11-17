b0f1a11b38b227db1b205b88ba19b8d85637ba84
Brittany Normandin <bnormandin@my365.bellevue.edu>
2021-11-17 18:10:07 -0500

    Update mongodb.py

--- module_5/mongodb.py
+++ module_5/mongodb.py
@@ -1,1 +1,14 @@
+# Brittany Normandin, CSD 310, 11/17/2021
+#Module 5 Assignment 5.2
 
+from pymongo import MongoClient
+
+uri="mongodb+srv://admin:admin@cluster0.9s1rm.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+# connect to MongoDB, change the << MONGODB URL >> to reflect your own connection string
+
+client = MongoClient(uri,  tls=True, tlsAllowInvalidCertificates=True)
+
+db = client.pytech
+
+print(db.list_collection_names())
